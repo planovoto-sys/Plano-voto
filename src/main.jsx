9e7cd45ae0112b6e7b1b88fd142f0d258b1aadf8
Alexandre Hackbardt Bolsoni <104595251+AlexandreBolsoni@users.noreply.github.com>
2026-04-02 10:34:03 -0300

    correçoes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
-import { UserProvider } from './contexts/UserContext';
+import React from 'react'; 
 import { createRoot } from 'react-dom/client';
-// ...
+import App from './App'; 
+import { UserProvider } from './contexts/UserContext';
+
+// ... (seus outros imports, como o index.css)
+// Certifique-se de que o caminho para o index.css esteja correto
+import './index.css';
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserProvider>
