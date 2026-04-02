f53e032f99c1a51d6caa4f89103f72be889ffea0
Alexandre Hackbardt Bolsoni <104595251+AlexandreBolsoni@users.noreply.github.com>
2026-04-02 10:27:39 -0300

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { UserProvider } from './contexts/UserContext';
-
+import { createRoot } from 'react-dom/client';
 // ...
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
